9f4e3551c023dece675c61d44787fa3b6f539596
Toukir <to.toukir.rahman@gmail.com>
2024-02-26 00:19:37 +0600

    no message

--- js/main.js
+++ js/main.js
@@ -38,6 +38,67 @@
 			}
 		});
 	});
+
+
+	// Img Popup==========>
+	$(document).ready(function() {
+		var modal = $('#modalimg');
+		var modalImg = $('.modalim-content');
+		var modalDescription = $('.modal-description');
+		var closeBtn = $('.close');
+		var prevBtn = $('.prev');
+		var nextBtn = $('.next');
+		var totalThumbnails = $('.thumbnail').length;
+	
+		$('.thumbnail').click(function() {
+			var imgSrc = $(this).find('img').attr('src');
+			var imgDesc = $(this).find('.description').html(); // Use .html() to get the HTML content of the description
+			modalImg.attr('src', imgSrc);
+			modalDescription.html(imgDesc); // Set HTML content of modal description
+			modal.css('display', 'flex').hide().fadeIn(); // Open modal with fade-in animation
+			$('body').css('overflow-y', 'hidden'); // Prevent scrolling of body content
+		});
+	
+		closeBtn.click(function() {
+			modal.fadeOut(function() {
+				$(this).css('display', 'none'); // Hide modal with fade-out animation
+			});
+			$('body').css('overflow-y', 'auto'); // Revert body overflow to auto
+		});
+	
+		prevBtn.click(function() {
+			var currentThumbnail = parseInt(modalImg.attr('src').replace(/^.*[\\\/]/, '').replace(/\..+$/, ''));
+			var prevThumbnailIndex = (currentThumbnail - 2 + totalThumbnails) % totalThumbnails; // Subtract 2 to go to the previous thumbnail index, considering 0-based index and 1-based image numbering
+			var prevThumbnail = $('#thumbnail' + (prevThumbnailIndex + 1)); // Add 1 to the index to match the thumbnail numbering
+			var imgSrc = prevThumbnail.find('img').attr('src');
+			var imgDesc = prevThumbnail.find('.description').html(); // Use .html() to get the HTML content of the description
+			modalImg.attr('src', imgSrc);
+			modalDescription.html(imgDesc); // Set HTML content of modal description
+		});
+	
+		nextBtn.click(function() {
+			var currentThumbnail = parseInt(modalImg.attr('src').replace(/^.*[\\\/]/, '').replace(/\..+$/, ''));
+			var nextThumbnailIndex = (currentThumbnail % totalThumbnails); // If we reach the last thumbnail, go back to the first one
+			var nextThumbnail = $('#thumbnail' + (nextThumbnailIndex + 1)); // Add 1 to the index to match the thumbnail numbering
+			var imgSrc = nextThumbnail.find('img').attr('src');
+			var imgDesc = nextThumbnail.find('.description').html(); // Use .html() to get the HTML content of the description
+			modalImg.attr('src', imgSrc);
+			modalDescription.html(imgDesc); // Set HTML content of modal description
+		});
+	
+		$(window).click(function(event) {
+			if (event.target == modal[0]) {
+				modal.fadeOut(function() {
+					$(this).css('display', 'none'); // Hide modal with fade-out animation if clicked outside the modal
+				});
+				$('body').css('overflow-y', 'auto'); // Revert body overflow to auto
+			}
+		});
+	});
+	
+	
+	
+	
 	
 })(jQuery);
 
